b70a65f797d55a99731997379c73175778046334
TLTechbender <myjoyhasarrived2004@gmail.com>
2023-02-22 04:06:54 +0100

    create:static asset, halfway done

--- server.js
+++ server.js
@@ -3,6 +3,10 @@ const fs = require("fs");
 
 const server = http.createServer((req, res) => {
   // Your code here
+  const staticPage= fs.readFileSync('index.html');
+  res.statusCode=200;
+  res.setHeader=('Content-Type', 'text/html');
+  return res.end(staticPage);
 });
 
 const port = 5000;
